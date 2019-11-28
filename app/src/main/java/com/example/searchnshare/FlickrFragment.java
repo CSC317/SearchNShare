1c935cf37a0a89904c020dee921866511f90993a
bsingh1997 <bsingh1997@email.arizona.edu>
2019-11-27 20:55:38 -0700

    basic main menu and fragments done. forward and back buttons not implemented.

---
+++ app/src/main/java/com/example/searchnshare/FlickrFragment.java
@@ -0,0 +1,39 @@
+package com.example.searchnshare;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FlickrFragment extends Fragment {
+
+    public Activity containerActivity = null;
+
+    public FlickrFragment() {}
+
+    /**
+     * Sets the container activity to the activity passed as a parameter.
+     *
+     * @param containerActivity the current activity.
+     */
+    public void setContainerActivity(Activity containerActivity) {
+        this.containerActivity = containerActivity;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_flickr, container, false);
+    }
+
+}
