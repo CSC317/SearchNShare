9b8298411cca16bc06ff93ff2af66705d31a8faa
bsingh1997 <bsingh1997@email.arizona.edu>
2019-12-11 14:40:47 -0700

    added help button and page

---
+++ app/src/main/java/com/example/searchnshare/HelpFragment.java
@@ -0,0 +1,30 @@
+package com.example.searchnshare;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HelpFragment extends Fragment {
+
+
+    public HelpFragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_help, container, false);
+        return v;
+    }
+
+}
